4cb11bd32213f1535aef8c3a1b24493ad19c8a91
Sam Lipson <slipson@ycp.edu>
2013-02-05 10:05:15 -0500

    initial checkin

---
+++ ZombieProjectModelClasses/src/zombieProject/Game.java
@@ -0,0 +1,15 @@
+package zombieProject;
+
+public class Game {
+	
+	public static void main(String[] args){
+		Player p = new Player(0, 0);
+		
+		
+		
+		
+		
+		
+	}
+	
+}
---
+++ ZombieProjectModelClasses/src/zombieProject/Player.java
@@ -0,0 +1,24 @@
+package zombieProject;
+
+import java.util.Scanner;
+
+public class Player{
+
+	private double x;
+	private double y;
+	
+	public Player(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX(){
+		return x;
+	}
+	
+	public double getY(){
+		return y;
+	}
+	
+	
+}
