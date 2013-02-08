33477933b50906204002ca36a96a73b7a8146816
Shane Bonner <sbonner1@ycp.edu>
2013-02-07 19:47:41 -0500

    new Zombie class, addition to Player and Game

---
+++ ZombieProjectModelClasses/src/Zombie.java
@@ -0,0 +1,41 @@
+
+public class Zombie {
+
+		private double x;
+		private double y;
+		
+		private double health;
+		
+		public Zombie(double x, double y){
+			this.x = x;
+			this.y = y;
+			
+			health = 50;
+		}
+		
+		public double getX(){
+			return x;
+		}
+		
+		public void setX(double x){
+			this.x = x;
+		}
+		
+		public double getY(){
+			return y;
+		}
+		
+		public void setY(double y){
+			this.y = y;
+		}
+		/** 
+		 * A Zombie's health will decrease IF the player's
+		 * bullet's coordinates are the same as the Zombie's.
+		 * In other words, Zombie got shot.
+		 * 
+		 * @param val
+		 */
+		public void decreaseHealth(double val){
+			this.health -= val;
+		}
+}
--- ZombieProjectModelClasses/src/zombieProject/Player.java
+++ ZombieProjectModelClasses/src/zombieProject/Player.java
@@ -7,9 +7,13 @@ public class Player{
 	private double x;
 	private double y;
 	
+	private double health;
+	
 	public Player(double x, double y){
 		this.x = x;
 		this.y = y;
+		
+		health = 100;
 	}
 	
 	public double getX(){
@@ -28,9 +32,17 @@ public class Player{
 		this.y = y;
 	}
 	
+	public void increaseHealth(double val){
+		this.health += val;
+	}
+	
+	public void decreaseHealth(double val){
+		this.health -= val;
+	}
+	
 	//check if collision possible
 	//if not allow movement
-	//This is for all yall
+	//This is for all y'all
 	
 	
 	
