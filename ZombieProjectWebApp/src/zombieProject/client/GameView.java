4c34578fb8cb2c27536f267ed6e5fe2f79385785
Sam Lipson <slipson@ycp.edu>
2013-03-01 11:09:53 -0500

    before the storm

--- ZombieProjectWebApp/src/zombieProject/client/GameView.java
+++ ZombieProjectWebApp/src/zombieProject/client/GameView.java
@@ -1,19 +1,54 @@
 package zombieProject.client;
-
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.dom.client.Style.Unit;
 
-public class GameView extends Composite {
+public class GameView extends Composite{
+	
+	private Canvas canvas;
+	private Timer timer;
+	//private Game game;
+	
 	public GameView() {
 		
 		LayoutPanel layoutPanel = new LayoutPanel();
 		initWidget(layoutPanel);
 		
-		InlineLabel nlnlblThisIsThe = new InlineLabel("This is the game!");
-		layoutPanel.add(nlnlblThisIsThe);
-		layoutPanel.setWidgetLeftWidth(nlnlblThisIsThe, 109.0, Unit.PX, 202.0, Unit.PX);
-		layoutPanel.setWidgetTopHeight(nlnlblThisIsThe, 25.0, Unit.PX, 18.0, Unit.PX);
+//		InlineLabel nlnlblThisIsThe = new InlineLabel("This is the game!");
+//		layoutPanel.add(nlnlblThisIsThe);
+//		layoutPanel.setWidgetLeftWidth(nlnlblThisIsThe, 109.0, Unit.PX, 202.0, Unit.PX);
+//		layoutPanel.setWidgetTopHeight(nlnlblThisIsThe, 25.0, Unit.PX, 18.0, Unit.PX);
+		
+		this.canvas = Canvas.createIfSupported();
+		canvas.setWidth("800px");
+		canvas.setHeight("600px");
+		layoutPanel.add(canvas);
+		layoutPanel.setWidgetLeftWidth(canvas, 20.0, Unit.PX, 800.0, Unit.PX);
+		layoutPanel.setWidgetTopHeight(canvas, 100, Unit.PX, 600.0, Unit.PX);
+		
+		timer = new Timer() {
+			@Override
+			public void run() {
+				handleTimerTick();
+			}
+		};
+		timer.scheduleRepeating(1000 / 60);
+	}
+
+	protected void handleTimerTick() {
+		paint();
+	}
+
+	private void paint() {
+		// TODO: use Game object to determine what to draw
+		canvas.getContext2d().setFillStyle("#008600");//zombie color
+		canvas.getContext2d().fillRect(10.0, 10.0, 4.0, 4.0);//x and y; width and height
+		
+		canvas.getContext2d().setFillStyle("#FFCC99");//human color
+		canvas.getContext2d().fillRect(100.0, 10.0, 4.0, 4.0);//x and y; width and height
+		
 	}
 }
